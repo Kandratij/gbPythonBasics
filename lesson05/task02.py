a1f887b1bde755d7b8bf83bcfdedbd7d02807358
Kandratij <andrey.ganyushkin@gmail.com>
2022-01-21 16:27:13 +0300

    ДЗ Урок 5. Генераторы и comprehensions. Множества

---
+++ lesson05/task02.py
@@ -0,0 +1,15 @@
+# *(вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
+max_num = 15
+odd_nums = (num for num in range(1, max_num+1, 2))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
+
+
